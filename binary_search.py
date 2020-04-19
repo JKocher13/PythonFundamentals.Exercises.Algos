2eee7c29262f721970adf3fbfe29d0a00e5b5079
James Kocher <JKocher13@yahoo.com>
2020-04-19 16:32:26 -0400

    tests added, not complete) q
    sef)

--- binary_search.py
+++ binary_search.py
@@ -3,33 +3,43 @@ import typing
 #def binary_search(list_in: List[int], item: int) -> int:
 def binary_search(list_in, item):
 	if item in list_in:
-		length_index = len(list_in)-1
-		sorted_list_in = sorted(list_in)
-		if len(list_in) == 1:
-			mid = 0
-		else:
-			mid = length_index//2
+		mid = determine_mid(list_in)
 
 		if item == list_in[mid]:
 			print("Number is " + str(list_in[mid]))
 			return list_in[mid]
 
-		elif item > list_in[mid]:
-			new_list = list_in[mid+1:]
-			print(str(list_in[mid]) + " is too low")
-			print(new_list)
-			binary_search(new_list,item)
+		if item > list_in[mid]:
+			lst = binary_test_low(list_in,mid)
+			binary_search(lst,item)
 
-		elif item < list_in[mid]:
-			new_list = list_in[0:mid]
-			print(str(list_in[mid]) + " is too high")
-			print(new_list)
-			binary_search(new_list,item)
+		if item < list_in[mid]:
+			lst = binary_test_high(list_in,mid)
+			binary_search(lst,item)
 
 	else:
+		print('item not in list')
 		return(-1)
 
 
+def binary_test_low(list_in,mid):
+	new_list = list_in[mid:]
+	print(str(list_in[mid]) + " is too low")
+	return new_list
 
-list_in = list(range(1,101))
-binary_search(list_in, 1)
+def binary_test_high(list_in,mid):
+	new_list = list_in[0:mid]
+	print(str(list_in[mid]) + " is too high")
+	return new_list
+
+def determine_mid(list_in):
+	length_index = len(list_in)
+	list_in = sorted(list_in)
+	if len(list_in) == 1:
+		mid = 0
+	else:
+		mid = length_index//2
+	return mid
+
+lst = list(range(1,101))
+binary_search(list(range(1,101)), 52)
---
+++ test_binary_search.py
@@ -0,0 +1,54 @@
+import unittest
+import unittest.mock
+import binary_search
+import sys
+import io
+from unittest import mock
+from unittest.mock import patch
+
+
+class TestFunctions(unittest.TestCase):
+    def __int__(self):
+    	pass
+
+    def setUp(self):
+    	self._list1 = list(range(1,101))
+    	self._list2 = [1,5,25,100,42,300,5,0,500]
+    	self._item1 = 75
+    	self._item2 = 51
+    	self._item3 = 25
+    	self._item4 = 1
+    	self._item5 = 100
+    	self._item6 = 50
+
+    @patch("binary_search.binary_test_high")
+    def test_high_value(self, mock_high_test):
+    	text_trap = io.StringIO()
+    	sys.stdout = text_trap
+    	binary_search.binary_search(self._list1, self._item1)
+    	self.assertTrue(mock_high_test)
+
+    @patch("binary_search.binary_test_low")
+    def test_low_value(self, mock_low_test):
+    	text_trap = io.StringIO()
+    	sys.stdout = text_trap
+    	binary_search.binary_search(self._list1, self._item3)
+    	self.assertTrue(mock_low_test)
+
+    def test_binary_search(self):
+    	text_trap = io.StringIO()
+    	sys.stdout = text_trap
+    	self.assertEqual(binary_search.binary_search(self._list1, self._item2),51)
+
+    def test_low_return(self):
+    	text_trap = io.StringIO()
+    	sys.stdout = text_trap
+    	self.assertEqual(binary_search.binary_test_low(self._list1, self._item6),list(range(51,101)))
+
+    def test_high_return(self):
+    	text_trap = io.StringIO()
+    	sys.stdout = text_trap
+    	self.assertEqual(binary_search.binary_test_high(self._list1, self._item6),list(range(1,51)))
+
+if __name__ == '__main__':
+    unittest.main()
